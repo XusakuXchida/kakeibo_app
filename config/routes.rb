467ff9887eba4d8b5ea8c5e92eaa8b662046405f
YUSAKU UCHIDA <ni34ga@yahoo.co.jp>
2018-12-18 01:18:46 +0900

    make account form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  resources :home
+  resources :accounts
+  # get 'accounts/index'
+  # get 'home/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
