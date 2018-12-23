f70151826cacb4818a50207b6888413d8f245128
YUSAKU UCHIDA <ni34ga@yahoo.co.jp>
2018-12-24 01:41:11 +0900

    make history show page

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,26 +1,17 @@
 class AccountsController < ApplicationController
   def index
     @today = Date.today
-    beginning_of_month = @today.beginning_of_month
-    @accounts = Account.all.search_by_month(beginning_of_month)
-    @sum_money = 0
-    @sum_money_red = 0
-    @sum_money_yellow = 0
-    @sum_money_green = 0
+    # beginning_of_month = @today.beginning_of_month
+    @accounts = Account.all.search_by_month(@today.beginning_of_month)
+    set_sum_money
 
     @categories = Category.all
   end
 
   def create
-    account = Account.new
-    account.date = params[:date]
-    account.year = params[:date].to_date.year
-    account.month = params[:date].to_date.month
-    account.day = params[:date].to_date.day
-    account.category = params[:category]
-    account.purpose = params[:purpose]
-    account.money = params[:money]
-    if account.save
+    @account = Account.new
+    get_params
+    if @account.save
       redirect_to accounts_path
     else
       redirect_to "/home"
@@ -33,15 +24,9 @@ class AccountsController < ApplicationController
   end
 
   def update
-    account = Account.find(params[:id])
-    account.date = params[:date]
-    account.year = params[:date].to_date.year
-    account.month = params[:date].to_date.month
-    account.day = params[:date].to_date.day
-    account.category = params[:category]
-    account.purpose = params[:purpose]
-    account.money = params[:money]
-    if account.save
+    @account = Account.find(params[:id])
+    get_params
+    if @account.save
       redirect_to accounts_path
     else
       redirect_to "/home"
@@ -53,4 +38,23 @@ class AccountsController < ApplicationController
     account.destroy
     redirect_to accounts_path
   end
+
+  private
+
+    def set_sum_money
+      @sum_money = 0
+      @sum_money_red = 0
+      @sum_money_yellow = 0
+      @sum_money_green = 0
+    end
+
+    def get_params
+      @account.date = params[:date]
+      @account.year = params[:date].to_date.year
+      @account.month = params[:date].to_date.month
+      @account.day = params[:date].to_date.day
+      @account.category = params[:category]
+      @account.purpose = params[:purpose]
+      @account.money = params[:money]
+    end
 end
--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -4,6 +4,19 @@ class HistoriesController < ApplicationController
   end
 
   def show
-    @accounts = Account.find(params[:id])
+    year_month = params[:id].split("-")
+    @year = year_month[0]
+    @month = year_month[1]
+    @accounts = Account.search_by_year_and_month(@year, @month)
+    set_sum_money
   end
+
+  private
+
+    def set_sum_money
+      @sum_money = 0
+      @sum_money_red = 0
+      @sum_money_yellow = 0
+      @sum_money_green = 0
+    end
 end
